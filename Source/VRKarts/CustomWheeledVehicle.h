6401ca6ed20376efcb4c7021b2511b0b914dcec3
Daniel Tebbutt <moeruze.burning@gmail.com>
2015-03-16 21:20:42 +1300

    First play around with codez

---
+++ Source/VRKarts/CustomWheeledVehicle.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/WheeledVehicle.h"
+#include "CustomWheeledVehicle.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRKARTS_API ACustomWheeledVehicle : public AWheeledVehicle
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/VRKarts/VRKarts.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VRKarts.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VRKarts, "VRKarts" );
